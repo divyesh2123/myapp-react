861bec4a99dee9824b8b0421cb510b8cd05e5536
divyesh2123 <divyesh2198@gmail.com>
2023-08-23 09:18:06 +0530

    changes the for master

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -51,27 +51,7 @@ export class AccountComponent implements OnInit, AfterViewInit{
   
  
 
-  open(content: any) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
-	}
-
-	private getDismissReason(reason: any): string {
-		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
-
+ 
 	saveInformation()
 	{
 		this.myOutput.emit(this.myform.value);
@@ -81,7 +61,7 @@ export class AccountComponent implements OnInit, AfterViewInit{
 	opentest(index:any)
 	{
 		console.log(index);
-		this.open(content as any);
+		
 	}
 
 
